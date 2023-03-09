503a6a172526e385789d5acbaee4610b1ed5bb44
AlexWatabe9 <alexwatabe@protonmail.com>
2023-03-08 20:05:06 -0700

    added reset css

--- script.js
+++ script.js
@@ -1,59 +1,4 @@
-// git add .
-// git commit -m ""
-// git push origin {branch name} 
-
-
-// git status (checks the branch you are on )
-// git switch {name of branch} (will tell you the name of your branch)
-
-//function call each api
-// separate api each function
-//init to call multiple 
-
-
-// var url = "http://numbersapi.com/number/type";
-
-
-// var random = document.getElementById("random");
-
-// function getApi() {
-//     var url = "http://numbersapi.com/42/trivia";
-
-//     fetch(url)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         })
-//         .catch(function (error) {
-//             console.error(error);
-//         });
-// }
-
-// getApi();
-
-
-// fetch(url)
-//     .then(function (response) {
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     })
-//     .catch(function (error) {
-//         console.error(error);
-//     });
-
-
-
-// factButton.addEventListener("click", async () => {
-//   var response = await fetch("https://go-apod.herokuapp.com/apod");
-//   var fact = await response.text();
-//   factDisplay.innerText = fact;
-// });
-var random = document.getElementById("random-button")
+var jokes = document.getElementById("jokes-button")
 
 async function fetchJoke() {
     // API endpoint URL
@@ -88,4 +33,13 @@ fetchJoke()
   console.error(error);
 });
 
-random.addEventListener('click, fetchJoke');
+
+
+
+
+
+
+
+
+jokes.addEventListener('click', fetchJoke);
+console.log(jokes);
