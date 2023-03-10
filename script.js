a3d5abc305d6759a37e04f88e50645bd7c4ca2aa
kpetrosky <kpetrosky36@gmail.com>
2023-03-09 19:34:05 -0700

    updated joke api

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-var jokesButton = document.getElementById("jokes-button");
-var displayEl = document.getElementById("displayText");
-async function fetchJoke() {
-  // API endpoint URL
+function fetchJoke() {
+  // API URL
   var url =
     "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
-
   // Make API request
-  var requestApi = await fetch(url);
-
+  var requestApi = fetch(url);
   // Get the JSON response
-  var response = await requestApi.json();
-  var jokeText = "";
+  var response = requestApi.json();
   // Return the setup and punchline of the joke
   if (response["type"] === "twopart") {
     return [response["setup"], response["delivery"]];
@@ -18,29 +13,17 @@ async function fetchJoke() {
     return [response["joke"], null];
   }
 }
-
-// Fetch a random joke from the API
-fetchJoke()
-  .then(([setup, punchline]) => {
-    // Display the setup of the joke
-    console.log(setup);
-
-    // If the joke is a two-part joke, display the punchline too
-    if (punchline) {
-      console.log(punchline);
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-
-    var displayJoke = document.createElement("p");
-    displayJoke.innerText = jokeText;
-    displayEl.innerHTML = "";
-    displayEl.append(displayJoke);
-  });
-
-jokesButton.addEventListener("click", fetchJoke);
-
+jokesButton.addEventListener("click",fetchJoke)
+function render(data) {
+  var jokeEl = document.createElement("p");
+  jokeEl.textContent = data;
+  if (punchline) {
+    var punchlineEl = document.createElement("p");
+    punchlineEl.textContent = punchline;
+    displayEl.appendChild(punchlineEl);
+  }
+  displayEl.appendChild(jokeEl);
+}
 
 //------------ numbers api
 
