b00068f5bfad3d2e63e8a3d6eb27ad7b755b5cf9
AlexWatabe9 <alexwatabe@protonmail.com>
2023-03-09 19:40:49 -0700

    accepting incoming changes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var jokesButton = document.getElementById("jokes-button");
 var displayEl = document.getElementById("displayText");
-async function fetchJoke() {
+
+function fetchJoke() {
   // API endpoint URL
   var url =
     "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
   // Make API request
-  var requestApi = await fetch(url);
+  var requestApi = fetch(url);
 
   // Get the JSON response
-  var response = await requestApi.json();
-  var jokeText = "";
+  var response = requestApi.json();
+
   // Return the setup and punchline of the joke
   if (response["type"] === "twopart") {
     return [response["setup"], response["delivery"]];
@@ -19,37 +20,21 @@ async function fetchJoke() {
   }
 }
 
-// Fetch a random joke from the API
-fetchJoke()
-  .then(([setup, punchline]) => {
-    // Display the setup of the joke
-    console.log(setup);
-  })
-    //function for data 
-    .then(function (data) {
-      //looping over the fetch response and inserting the URL of your repos into a list
-      for (var i = 0; i < data.length; i++) {
-        //Create a paragraph element
-        var displayJoke = document.createElement('p');
-    
-        //Set the text of the paragraph element to the JSON response's .html_url property
-        displayJoke.textContent = data[i].html_url;
-    
-        //Append the paragraph element to the HTML element with the id 'displayEl'
-        displayEl.appendChild(displayJoke);
-      }
-    
+jokesButton.addEventListener("click",fetchJoke)
 
-    // If the joke is a two-part joke, display the punchline too
-    if (punchline) {
-      console.log(punchline);
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+function render(data) {
+  var jokeEl = document.createElement("p");
+  jokeEl.textContent = data;
 
-jokesButton.addEventListener("click", fetchJoke);
+  if (punchline) {
+    var punchlineEl = document.createElement("p");
+    punchlineEl.textContent = punchline;
+    displayEl.appendChild(punchlineEl);
+  }
+
+  displayEl.appendChild(jokeEl);
+}
+
 
 
 //------------ numbers api
