13f23c0ea5254f215955ea85b5d14f90d5d46bee
Ani-Kandikattu <120747994+Ani-Kandikattu@users.noreply.github.com>
2023-03-14 22:15:46 -0700

    Reworked cats and memes to enable saving and retrieving

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Global Variables
 var randomButton = document.getElementById("random-button");
-var numberButton = document.getElementById("numbers-button");
+var numberButton = document.getElementById("number-button");
 var displayText = document.getElementById("content");
 var jokesButton = document.getElementById("jokes-button");
 var dogBtn = document.getElementById("dog-button");
@@ -11,6 +11,9 @@ var displayText = document.getElementById("content");
 var resetButton = document.getElementById("reset");
 var displayPic = document.getElementById("content");
 var likeButton = document.getElementById("like");
+var favoritebutton = document.querySelector("#favorite");
+
+//Reset Button to refresh the page, clearing the content
 
 resetButton.addEventListener("click", function () {
   location.reload();
@@ -18,10 +21,9 @@ resetButton.addEventListener("click", function () {
 
 function refresh() {
   location.reload();
-} 
+}
 
-
-//Jokes
+//API to fetch Jokes
 
 function fetchJoke() {
   var url =
@@ -38,8 +40,6 @@ function fetchJoke() {
       console.log(data);
       renderJoke(data);
     });
-
-  // jokesButton.addEventListener("click",fetchJoke)
 }
 function renderJoke(data) {
   var jokeEl = document.createElement("p");
@@ -53,7 +53,8 @@ function renderJoke(data) {
   }
 }
 
-//Memes Button for random memes
+//API to fetch meme templates
+
 function fetchMemes() {
   var url = "https://api.imgflip.com/get_memes";
   fetch(url)
@@ -69,11 +70,14 @@ function fetchMemes() {
       var memes = data.data.memes;
       var randomMeme = Math.floor(Math.random() * memes.length);
       var meme = memes[randomMeme];
-      displayPic.innerHTML = `<img src="${meme.url}" alt="${meme.name}">`;
+      var paragraph = document.createElement("p");
+      paragraph.innerHTML = `<img src="${meme.url}" alt="${meme.name}">`;
+      displayPic.appendChild(paragraph);
     });
 }
 
-//Api to fetch cat facts
+//API to fetch cat facts
+
 function fetchCatFacts() {
   var url = "https://catfact.ninja/fact?max_length=140";
   fetch(url)
@@ -96,7 +100,8 @@ function fetchCatFacts() {
   }
 }
 
-//Api to fetch dog pics
+//API to fetch dog pics
+
 function fetchDogs() {
   var url = "https://random.dog/woof.json?ref=apilist.fun";
 
@@ -120,10 +125,7 @@ function fetchDogs() {
   }
 }
 
-//------------ numbers api
-
-var numberButton = document.getElementById("number-button");
-var displayText = document.getElementById("content");
+//API to fetch number facts
 
 function fetchNumber() {
   var number = Math.floor(Math.random() * 100);
@@ -149,26 +151,8 @@ function render(data) {
   displayText.appendChild(facts);
 }
 
-//Cat Button for pictures of cats
-// function fetchCats() {
-//   var url = "https://cataas.com/cat";
+//Api to fetch cat pics
 
-//   fetch(url)
-//     .then((response) => {
-//       if (response.ok) {
-//         //Used .blob instead of .json because the API returns an image file, not JSON data.
-//         //https://developer.mozilla.org/en-US/docs/Web/API/Response/blob
-//         return response.blob();
-//       } else {
-//         Error("Something doesn't work!");
-//       }
-//     })
-//     .then((myBlob) => {
-//       var imageUrl = URL.createObjectURL(myBlob);
-//       displayPic.innerHTML = `<img src= "${imageUrl}" alt="Random cat image">`;
-//       console.log(myBlob)
-//     });
-//   };
 function fetchCats() {
   var url = "https://cataas.com/cat";
 
@@ -189,12 +173,12 @@ function fetchCats() {
 
   function render(imageUrl) {
     var paragraph = document.createElement("p");
-    displayPic.innerHTML = `<img src= "${imageUrl}" alt="Random cat image">`;
+    paragraph.innerHTML = `<img src= "${imageUrl}" alt="Random cat image">`;
     displayPic.appendChild(paragraph);
   }
 }
+
 //recipes
-// var foodButton = document.querySelector('#food')
 
 function fetchFood() {
   // var url = "https://www.themealdb.com/api/json/v1/1/categories.php";
@@ -232,24 +216,18 @@ function fetchFood() {
 
 var foodButton = document.querySelector("#food-button");
 
-//favorite button
-// var userInput = document.querySelector('userinput').value
-
-var favoritebutton = document.querySelector("#favorite");
+//Function to save liked content to local storage
 
 favoritebutton.addEventListener("click", function () {
-  //    var userInput = document.querySelector('userinput').value
   var content = document.querySelector("#content");
-  // console.log(content.children[1].children[0].src)
-  var history = JSON.parse(localStorage.getItem("history")) || []
+  var history = JSON.parse(localStorage.getItem("history")) || [];
   var saveContent = content.children[1].children[0];
   var otherContent = content.children[1];
-  if (typeof saveContent !== 'undefined' && saveContent.tagName === "IMG") {
+  if (typeof saveContent !== "undefined" && saveContent.tagName === "IMG") {
     //if we get remove p tag with place holder change first child to index of 0
     // console.log("I made it");
     history.push(content.children[1].children[0].src);
-  }
-   else {
+  } else {
     history.push(content.children[1].textContent);
   }
   localStorage.setItem("history", JSON.stringify(history));
@@ -264,57 +242,49 @@ function checkUrl(string) {
   }
 }
 
+//Function to retrieve favorited content from local storage
+
 function getLikes() {
   var content = document.querySelector("#content");
-  var myLikes = JSON.parse(localStorage.getItem('history'));
-  for (var i = 0; i < myLikes.length; i++ ){
-    if (checkUrl(myLikes[i])){ 
-      var myImage = document.createElement('img')
+  var myLikes = JSON.parse(localStorage.getItem("history"));
+  for (var i = 0; i < myLikes.length; i++) {
+    if (checkUrl(myLikes[i])) {
+      var myImage = document.createElement("img");
       myImage.src = myLikes[i];
-      // set the source of the image href = i 
+      // set the source of the image href = i
       displayText.appendChild(myImage);
-      
     } else {
-      // build paragraph tag and set content 
+      // build paragraph tag and set content
       var like = document.createElement("p");
       like.textContent = myLikes[i];
       displayText.appendChild(like);
     }
-  
   }
 
-  render(localStorage)
+  render(localStorage);
 }
 
-function fetchRandom(){
-var apiList=[
-  'fetchJoke','fetchDogs','fetchFact', 'fetchCatFacts', 'fetchCats'
-]
-var i = apiList[Math.floor(Math.random()* apiList.length)]
-console.log(i)
-if (i==='fetchJoke'){
-  fetchJoke();
+//Function for the Random option that chooses from all of the categories
+
+function fetchRandom() {
+  var apiList = [
+    "fetchJoke",
+    "fetchDogs",
+    "fetchFact",
+    "fetchCatFacts",
+    "fetchCats",
+  ];
+  var i = apiList[Math.floor(Math.random() * apiList.length)];
+  console.log(i);
+  if (i === "fetchJoke") {
+    fetchJoke();
+  } else if (i === "fetchDogs") {
+    fetchDogs();
+  } else if (i === "fetchNumber") {
+    fetchNumber();
+  } else if (i === "fetchCatFacts") fetchCatFacts();
+  else if (i === "fetchCats") fetchCats();
 }
-else if (i === 'fetchDogs'){
-  fetchDogs();
-}
-else if (i === 'fetchNumber'){
-  fetchNumber();
-}
-else if (i === 'fetchCatFacts')
-fetchCatFacts();
-else if (i === 'fetchCats')
-fetchCats();
-};
-
-
-
-
-
-
-
-
-
 
 // factsButton.addEventListener("click", fetchFact);
 dogBtn.addEventListener("click", fetchDogs);
